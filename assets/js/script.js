07fbbf8565d97e63f63d27fb5c3ac796273873bf
Kevin Taing <ktaing789@gmail.com>
2023-04-20 16:38:23 -0400

    functioning local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ $(function () {
     {id: "#hour-17", hour: 17}
   ];
 
-  let userEntry = [];
+  let getUserEntry = [];
 
   // ****** Time Variables ****** 
   let currentYear = dayjs().$y;
@@ -27,11 +27,11 @@ $(function () {
 
   // ****** Data Entry is saved on refresh ****** //
   if (localStorage.getItem("day-planner-events") !== null) {
-    userEntry = JSON.parse(localStorage.getItem("day-planner-events"));
+    getUserEntry = JSON.parse(localStorage.getItem("day-planner-events"));
 
-    localStorage.setItem("day-planner-events", JSON.stringify(userEntry));
+    localStorage.setItem("day-planner-events", JSON.stringify(getUserEntry));
 
-    $.each(userEntry, function (key, value) {
+    $.each(getUserEntry, function (key, value) {
       if (value.day === todaysDate) {
         let entryHourId = `#${value.hour}`;
         $(entryHourId).find("textarea").text(value.event);
@@ -57,4 +57,32 @@ $(function () {
     }
   });
 
+  // ****** Allows information to be saved in local storage ****** //
+  $(".saveBtn").click(function () {
+    let parentEl = $(this).parent().attr("id");
+    let parentElId = `#${parentEl}`;
+    let textBox = $(parentElId).find("textarea").val();
+    let userEntry = {
+      day: todaysDate,
+      hour: parentEl,
+      event: textBox,
+    };
+
+    if (localStorage.getItem("day-planner-events") !== null) {
+      getUserEntry = JSON.parse(localStorage.getItem("day-planner-events"));
+      getUserEntry.push(userEntry);
+      localStorage.setItem("day-planner-events", JSON.stringify(getUserEntry));
+
+      $.each(getUserEntry, function (key, value) {
+        if (value.day === todaysDate) {
+          entryHourId = `#${value.hour}`;
+          $(entryHourId).find("textarea").text(value.event);
+        }
+      });
+    }
+    else {
+      getUserEntry.push(userEntry);
+      localStorage.setItem("day-planner-events", JSON.stringify(getUserEntry));
+    }
+  });
 });
